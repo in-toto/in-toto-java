07602f62f4b9bd1d57f37eeb935bbc87baef551a
Santiago Torres <torresariass@gmail.com>
2018-07-02 14:51:33 -0400

    ENH: add boilerplate code

---
+++ src/main/java/io/in_toto/lib/App.java
@@ -0,0 +1,13 @@
+package io.in_toto.lib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
